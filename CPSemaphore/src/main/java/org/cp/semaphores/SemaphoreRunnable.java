a86c726ab76d7aeb7cffcd319a687d12fe9c3e6f
kornicameister <kornicameister@gmail.com>
2013-10-13 22:31:36 +0200

    initial architecture for semaphore task

--- CPSemaphore/src/main/java/org/cp/semaphores/SemaphoreRunnable.java
+++ CPSemaphore/src/main/java/org/cp/semaphores/SemaphoreRunnable.java
@@ -5,5 +5,9 @@ package org.cp.semaphores;
  * @version 0.0.1
  * @since 0.0.1
  */
-public interface SemaphoreRunnable {
+public interface SemaphoreRunnable
+        extends Runnable {
+    void initSemaphore(final int permits, final boolean fair);
+
+    void initStack(final int size);
 }
---
+++ CPSemaphore/src/main/java/org/cp/semaphores/threads/DFPSemaphore.java
@@ -0,0 +1,14 @@
+package org.cp.semaphores.threads;
+
+/**
+ * @author kornicameister
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+public class DFPSemaphore
+        extends FSemaphore {
+    @Override
+    protected void runInternal() {
+
+    }
+}
