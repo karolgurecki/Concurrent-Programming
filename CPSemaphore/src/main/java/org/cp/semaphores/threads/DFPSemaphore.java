dbff760f811024a801f4099a671baf1924169367
kornicameister <kornicameister@gmail.com>
2013-10-20 21:11:38 +0200

    repo initial draft

--- CPSemaphore/src/main/java/org/cp/semaphores/threads/DFPSemaphore.java
+++ CPSemaphore/src/main/java/org/cp/semaphores/threads/DFPSemaphore.java
@@ -1,11 +1,13 @@
 package org.cp.semaphores.threads;
 
 import com.google.common.base.Objects;
+import com.google.common.collect.Sets;
 import org.apache.log4j.Logger;
 import org.cp.semaphores.fractal.path.FractalPath;
 import org.cp.semaphores.tree.FractalDrawable;
 
 import java.util.Deque;
+import java.util.Set;
 
 /**
  * @author kornicameister
@@ -16,7 +18,8 @@ public class DFPSemaphore
         extends FSemaphore {
     private static final Logger LOGGER = Logger.getLogger(DFPSemaphore.class);
     private final FractalDrawable fractalDrawable;
-    private FractalPath lastPath = new FractalPath();
+    private FractalPath      lastPath   = new FractalPath();
+    private Set<FractalPath> drawnPaths = Sets.newHashSet();
 
     public DFPSemaphore(final Deque<FractalPath> resource, final FractalDrawable fractalDrawable) {
         super(resource);
@@ -28,10 +31,13 @@ public class DFPSemaphore
         LOGGER.info(String.format("Entering critical => %d", this.rounds.getRound()));
         while (this.resource.size() > 0) {
             final FractalPath path = this.resource.pollLast();
-            if (this.lastPath != null && !this.lastPath.equals(path)) {
-                LOGGER.debug(String.format("Polled value=%s", path));
+            if (!this.drawnPaths.contains(path)) {
+//                LOGGER.debug(String.format("Polled value=%s", path));
                 this.fractalDrawable.updateDrawing(path);
                 this.lastPath = path;
+                this.drawnPaths.add(path);
+            } else {
+                LOGGER.warn("Duplicate path detected...skipping");
             }
         }
         this.resource.offer(this.lastPath);
