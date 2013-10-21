cd64325d9eb1472642f279f79f6a9400ce46555b
karolgurecki <karolgurecki@gmail.com>
2013-10-21 21:36:37 +0200

    Initial CPMonitor commit

---
+++ CPMonitor/src/main/java/org/cp/monitor/CPMonitor.java
@@ -0,0 +1,13 @@
+package org.cp.monitor;
+
+/**
+ * Created by: Nappa
+ * Version: 0.01
+ * Since: 0.01
+ */
+public class CPMonitor {
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ CPMonitor/src/main/java/org/cp/monitor/core/CPLock.java
@@ -0,0 +1,12 @@
+package org.cp.monitor.core;
+
+import sun.misc.Lock;
+
+/**
+ * Created by: Nappa
+ * Version: 0.01
+ * Since: 0.01
+ */
+public class CPLock extends Lock {
+
+}
